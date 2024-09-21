eff081894252d8e49a63d9e6ea1042df07827d8b
wayne23123 <wayne23123@gmail.com>
2024-09-21 21:01:36 +0800

    修正FeaturedProducts頁面

--- src/components/FeaturedProducts.vue
+++ src/components/FeaturedProducts.vue
@@ -1,3 +1,76 @@
+<script setup>
+import { ElMessage } from 'element-plus';
+import 'element-plus/theme-chalk/el-message.css';
+import { useCartStore } from '../stores/cart';
+import { useProductionStore } from '../stores/production';
+
+// 使用 import 導入圖片
+import w001 from '../assets/imgs/w001.jpg';
+import w002 from '../assets/imgs/w002.jpg';
+import w003 from '../assets/imgs/w003.jpg';
+import { id } from 'element-plus/es/locales.mjs';
+import { ref } from 'vue';
+
+// 定義產品數據
+const products = [
+  {
+    id: 1,
+    name: '燈芯絨水手帽',
+    price: 1200,
+    image: w001,
+  },
+  {
+    id: 2,
+    name: 'Puma Suede',
+    price: 3000,
+    image: w002,
+  },
+  {
+    id: 3,
+    name: 'Champion運動外套',
+    price: 2100,
+    image: w003,
+  },
+];
+
+const amountDemo = ref(1);
+const sizeRef = ref('M');
+
+// 使用購物車 store
+const cartStore = useCartStore();
+const productionStore = useProductionStore();
+
+// 添加到購物車功能
+function addToCart(product) {
+  const targetProduct = productionStore.productions.find(
+    (item) => item.id == product.id
+  );
+  console.log('targetProduct', targetProduct);
+  // 添加到購物車
+  cartStore.emptyRefs.push({
+    id: targetProduct.id,
+    img: targetProduct.img,
+    title: targetProduct.title,
+    category: targetProduct.category,
+    star: targetProduct.star,
+    price: targetProduct.price,
+    description: targetProduct.description,
+    counter: amountDemo.value,
+    size: sizeRef.value,
+  });
+
+  cartStore.comparisonByIdFunction();
+
+  // 顯示成功消息
+  ElMessage({
+    message: `${product.name} 已成功加入購物車！`,
+    type: 'success',
+    duration: 2000, // 2 秒後消失
+    position: 'bottom',
+  });
+}
+</script>
+
 <template>
   <section class="featured-products">
     <h2>Today's Featured Products</h2>
@@ -10,28 +83,11 @@
         <img :src="product.image" alt="product image" />
         <h3>{{ product.name }}</h3>
         <p>{{ product.price }}</p>
-        <button>Add to Cart</button>
+        <button @click="addToCart(product)">Add to Cart</button>
       </div>
     </div>
   </section>
 </template>
-
-<script setup>
-// 使用 import 導入圖片
-import w001 from '../assets/imgs/w001.jpg';
-import w002 from '../assets/imgs/w002.jpg';
-import w003 from '../assets/imgs/w003.jpg';
-
-const products = [
-  {
-    name: 'Stylish T-shirt',
-    price: '$20',
-    image: w001,
-  },
-  { name: 'Cool Jacket', price: '$50', image: w002 },
-  { name: 'Comfy Shoes', price: '$80', image: w003 },
-];
-</script>
 
 <style scoped>
 .featured-products {
